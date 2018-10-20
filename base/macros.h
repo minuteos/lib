dc7edff1314768cc22ad9d633f482515a46f7779
Stefan Simek <simek@triaxis.sk>
2018-10-20 23:26:39 +0200

    base: macros.h - added optimization and BEGIN/END_EXTERN_C macros

--- base/macros.h
+++ base/macros.h
@@ -6,10 +6,39 @@
  * macros.h - unified macros across compilers
  */
 
+#pragma once
+
 #if __GNUC__
 #define ALWAYS_INLINE   __attribute__((always_inline))
 #define NO_INLINE       __attribute__((noinline))
-#else
+#endif
+
+#if __GNUC__ && !__clang__
+#define OPTIMIZE_SIZE   __attribute__((optimize("-Os")))
+#endif
+
+// safe fallbacks
+#ifndef ALWAYS_INLINE
 #define ALWAYS_INLINE   inline
+#endif
+
+#ifndef NO_INLINE
 #define NO_INLINE
+#endif
+
+#ifndef OPTIMIZE_SIZE
+#define OPTIMIZE_SIZE
+#endif
+
+#ifndef OPTIMIZE
+#define OPTIMIZE        OPTIMIZE_SIZE
+#endif
+
+// C/C++ agnostic EXTERN_C section
+#ifdef __cplusplus
+#define BEGIN_EXTERN_C  extern "C" {
+#define END_EXTERN_C    }
+#else
+#define BEGIN_EXTERN_C
+#define END_EXTERN_C
 #endif
