89a59a6acc2c4b9b5a8e6e5162590ddedb116647
Štefan Šimek <simek@triaxis.sk>
2019-10-04 23:30:06 +0200

    base: improve fix for unwanted Buffer constructor behavior by specializing the one case instead of introducing a copy constructor

--- targets/all/base/Span.h
+++ targets/all/base/Span.h
@@ -47,8 +47,6 @@ public:
     constexpr Span(const void* p, size_t len) : p((const char*)p), len(len) {}
     //! Constructs a Span covering a range of memory determined by start (inclusive) and end (exclusive)
     constexpr Span(const void* start, const void* end) : p((const char*)start), len((const char*)end - (const char*)start) {}
-    //! Constructs a Span that is a copy of another Span
-    constexpr Span(const Span& value) : p(value.p), len(value.len) {}
     //! Constructs a Span covering a single object
     template<class T> constexpr Span(const T& value) : p((const char*)&value), len(sizeof(T)) {}
 
@@ -259,10 +257,6 @@ public:
     constexpr Buffer(void* p, size_t len) : Span(p, len) {}
     //! Constructs a Buffer covering a range of memory determined by start (inclusive) and end (exclusive)
     constexpr Buffer(void* start, void* end) : Span(start, end) {}
-    //! Constructs a copy of a buffer object
-    constexpr Buffer(const Buffer& other) : Span(other.p, other.len) {}
-    //! Constructs a copy of a buffer object
-    constexpr Buffer(Buffer& other) : Span(other.p, other.len) {}
     //! Constructs a Buffer covering a single object
     template<class T> constexpr Buffer(T& value) : Span((char*)&value, sizeof(T)) {}
 
@@ -289,3 +283,5 @@ private:
 };
 
 ALWAYS_INLINE Buffer Span::CopyTo(Buffer buf) const { auto len = std::min(buf.len, this->len); memcpy(buf.Pointer(), this->p, len); return Buffer(buf.Pointer(), len); }
+
+template<> constexpr Buffer::Buffer<Buffer>(Buffer& value) : Span(value.pair) {}
