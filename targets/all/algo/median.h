512aea77bd857c2f4f001419d7381947ace1c4d9
Štefan Šimek <simek@triaxis.sk>
2019-12-10 16:33:09 +0100

    algo: fast 3- and 5-point median calculation

---
+++ targets/all/algo/median.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019 triaxis s.r.o.
+ * Licensed under the MIT license. See LICENSE.txt file in the repository root
+ * for full license information.
+ *
+ * algo/median.h
+ *
+ * Simple median implementations for fixed number of elements
+ */
+
+#include <base/base.h>
+
+template<typename T> T median(T a, T b, T c)
+{
+    return a > b
+        ? (c > a ? a : c > b ? c : b)
+        : (c > b ? b : c > a ? c : a);
+}
+
+template<typename T> T median(T a, T b, T c, T d, T e)
+{
+    T t;
+    if (a > b) { t = a; a = b; b = t; }
+    // a < b
+    if (c > d) { t = c; c = d; d = t; }
+    // c < d
+    if (a > c) { t = b; b = d; d = t; c = a; }
+    // b < c < d, a eliminated
+    if (e > b) { a = b; b = e; } else { a = e; }
+    // a < b
+    if (c > a) { t = b; b = d; d = t; a = c; }
+
+    return a < d ? a : d;
+}
