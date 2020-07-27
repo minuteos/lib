715e60d694547481f1888635464fd0d10ea74e19
Štefan Šimek <simek@triaxis.sk>
2020-07-27 07:12:06 +0200

    base: overflow - add overflow_t type for enforcing overflow comparison semantics

--- targets/all/base/overflow.h
+++ targets/all/base/overflow.h
@@ -26,4 +26,18 @@ template<typename T> constexpr T OVF_MIN(T a, T b) { return OVF_LT(a, b) ? a : b
 template<typename T> constexpr T OVF_MAX(T a, T b) { return OVF_GT(a, b) ? a : b; }
 template<typename T> constexpr std::make_signed_t<T> OVF_MAX_DIFF() { return std::numeric_limits<std::make_signed_t<T>>::max(); }
 
+template<typename T> struct overflow_t
+{
+    T value;
+
+    constexpr bool operator <(const overflow_t<T>& other) const { return OVF_LT(value, other.value); }
+    constexpr bool operator <=(const overflow_t<T>& other) const { return OVF_LE(value, other.value); }
+    constexpr bool operator >(const overflow_t<T>& other) const { return OVF_GT(value, other.value); }
+    constexpr bool operator >=(const overflow_t<T>& other) const { return OVF_GE(value, other.value); }
+    constexpr bool operator ==(const overflow_t<T>& other) const { return value == other.value; }
+    constexpr bool operator !=(const overflow_t<T>& other) const { return value != other.value; }
+
+    constexpr operator T() const { return value; }
+};
+
 #endif
