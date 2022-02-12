1527df490e1d36f74247209bbc89291d02487e6c
Štefan Šimek <simek@triaxis.sk>
2022-02-12 17:57:51 +0100

    kernel: async - do not destruct frame before calling epilog

--- targets/all/kernel/async.h
+++ targets/all/kernel/async.h
@@ -198,7 +198,7 @@ extern RES_PAIR_DECL(_async_epilog, AsyncFrame** pCallee, intptr_t result);
 }
 
 //! Finished the execution of an async function immediately and returns the specified value
-#define async_return(value) ({ f.~__FRAME(); return f.__epilog(__pCallee, (value)); })
+#define async_return(value) ({ auto __res = f.__epilog(__pCallee, (value)); f.~__FRAME(); return __res; })
 
 #define _async_yield(type, value) ({ __label__ next; (void)__async; f.__continue(&&next); return _ASYNC_RES(value, AsyncResult::type); next: false; })
 
